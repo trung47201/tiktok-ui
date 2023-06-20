14551a793041f42b2eb87e8ca95632b92126026f
Trung <101633153+trung47201@users.noreply.github.com>
2023-06-20 14:44:13 +0700

    Install and config prettier - code formatter

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import Button from '~/components/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <Button />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Button />
+        </div>
+    );
 }
 
 export default App;
